7987fc72a3349c974d9c18dfb7fb26e43b89268c
user1 <user1email@mail.ru>
2020-11-14 16:51:18 +0300

    first commit in br1.

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-arr1 = [1,2,3,4,5,6,7,8,9]
+arr1 = [11,12,13,14,15,16,17,18,19]
 
-def printer_minus_1(arr):
+def printer_plus_1(arr):
     for i in arr:
-        print(i - 1)
+        print(i + 1)
     return True
 
 if __name__ == "__main__":
-    printer_minus1(arr1)
+    printer_plus_1(arr1)
